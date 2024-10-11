8efa96f0286181ea0a5fd41ce89984f360530be0
Pavanrohith25 <151258697+Pavanrohith25@users.noreply.github.com>
2024-10-11 17:28:07 -0400

    Update authservice.js

--- Froentend/src/services/authservice.js
+++ Froentend/src/services/authservice.js
@@ -1,1 +1,38 @@
+import axios from 'axios';
 
+const API_URL = 'https://backend-api.com/api/auth';  
+
+const AuthService = {
+  login: async (email, password) => {
+    try {
+      const response = await axios.post(${API_URL}/login, {
+        email,
+        password,
+      });
+      
+      return response.data;
+    } catch (error) {
+      throw new Error('Login failed. Please check your credentials.');
+    }
+  },
+
+  register: async (name, email, password) => {
+    try {
+      const response = await axios.post(${API_URL}/register, {
+        name,
+        email,
+        password,
+      });
+      return response.data;
+    } catch (error) {
+      throw new Error('Registration failed. Please try again.');
+    }
+  },
+
+  logout: () => {
+  
+    localStorage.removeItem('userToken');
+  },
+};
+
+export default AuthService;
