2fff042ea4c55c699ecf0e64e7d336c380216d48
bnevimal3 <boppudivimal@gmail.com>
2024-10-11 17:20:39 -0400

    Update tabnavigator.js

--- Froentend/src/navigagtion/tabnavigator.js
+++ Froentend/src/navigagtion/tabnavigator.js
@@ -1,1 +1,31 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import ItineraryScreen from '../screens/ItineraryScreen';
+import BookingScreen from '../screens/BookingScreen';
 
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        options={{ tabBarLabel: 'Home' }} 
+      />
+      <Tab.Screen 
+        name="Itinerary" 
+        component={ItineraryScreen} 
+        options={{ tabBarLabel: 'Itinerary' }} 
+      />
+      <Tab.Screen 
+        name="Booking" 
+        component={BookingScreen} 
+        options={{ tabBarLabel: 'Booking' }} 
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigator;
