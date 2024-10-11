5069e55c8e8df3cbe72d735b28d35f8741f8bb03
PurveshKapadiya <146030138+PurveshKapadiya@users.noreply.github.com>
2024-10-11 17:03:28 -0600

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Trekora API!');
 });
 
-
 const PORT = process.env.PORT || 5000; // Use environment variable or default to 5000
 app.listen(PORT, () => {
   console.log(Server is running on http://localhost:${PORT});
