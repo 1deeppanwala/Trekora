085415fc831b4ee8ba3bd22c60e59e5d346d6de0
HiteshNaval <45076513+HiteshNaval@users.noreply.github.com>
2024-10-11 21:39:13 +0000

    Update store.js

--- Froentend/src/redux/store.js
+++ Froentend/src/redux/store.js
@@ -5,13 +5,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import userReducer from './userReducer';
 import itineraryReducer from './itineraryReducer';
 
-// Combine the reducers
 const rootReducer = combineReducers({
   user: userReducer,
   itinerary: itineraryReducer,
 });
 
-// Create the Redux store with middleware
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk)) 
