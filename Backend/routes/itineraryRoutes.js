c804bd405ae77d511ff89b93f3ef658ad720089a
bnevimal3 <boppudivimal@gmail.com>
2024-10-11 18:58:02 -0400

    Update itineraryRoutes.js

--- Backend/routes/itineraryRoutes.js
+++ Backend/routes/itineraryRoutes.js
@@ -9,16 +9,16 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-// Create a new itinerary
+
 router.post('/', authMiddleware, createItinerary);
 
-// Get all itineraries for a user
+
 router.get('/', authMiddleware, getItineraries);
 
-// Update an itinerary by ID
+
 router.put('/:id', authMiddleware, updateItinerary);
 
-// Delete an itinerary by ID
+
 router.delete('/:id', authMiddleware, deleteItinerary);
 
 module.exports = router;
