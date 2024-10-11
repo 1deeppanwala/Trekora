63263b08c38d03190c398a4473d7d5414e204fac
HiteshNaval <45076513+HiteshNaval@users.noreply.github.com>
2024-10-11 21:38:31 +0000

    Update store.js

--- Froentend/src/redux/store.js
+++ Froentend/src/redux/store.js
@@ -1,1 +1,20 @@
 
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import userReducer from './userReducer';
+import itineraryReducer from './itineraryReducer';
+
+// Combine the reducers
+const rootReducer = combineReducers({
+  user: userReducer,
+  itinerary: itineraryReducer,
+});
+
+// Create the Redux store with middleware
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)) 
+);
+
+export default store;
