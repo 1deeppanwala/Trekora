6ff9d487a596197ff291b12066a59de203c645a7
1deeppanwala <145871591+1deeppanwala@users.noreply.github.com>
2024-10-11 17:38:14 -0400

    Update itineraryReducer.js

--- Froentend/src/redux/itineraryReducer.js
+++ Froentend/src/redux/itineraryReducer.js
@@ -1,1 +1,51 @@
+const CREATE_ITINERARY = 'CREATE_ITINERARY';
+const DELETE_ITINERARY = 'DELETE_ITINERARY';
+const EDIT_ITINERARY = 'EDIT_ITINERARY';
+
+const initialState = {
+  itineraries: [],
+};
+
+const itineraryReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_ITINERARY:
+      return {
+        ...state,
+        itineraries: [...state.itineraries, action.payload], // add new itinerary
+      };
+    case DELETE_ITINERARY:
+      return {
+        ...state,
+        itineraries: state.itineraries.filter(
+          (itinerary) => itinerary.id !== action.payload
+        ), // remove itinerary by id
+      };
+    case EDIT_ITINERARY:
+      return {
+        ...state,
+        itineraries: state.itineraries.map((itinerary) =>
+          itinerary.id === action.payload.id ? action.payload : itinerary
+        ), // update itinerary
+      };
+    default:
+      return state;
+  }
+};
+
+export const createItinerary = (itinerary) => ({
+  type: CREATE_ITINERARY,
+  payload: itinerary,
+});
+
+export const deleteItinerary = (id) => ({
+  type: DELETE_ITINERARY,
+  payload: id,
+});
+
+export const editItinerary = (itinerary) => ({
+  type: EDIT_ITINERARY,
+  payload: itinerary,
+});
+
+export default itineraryReducer;
 
