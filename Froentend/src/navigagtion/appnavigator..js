7533975bd2894f16526aaaa9823e034c880cbde5
HiteshNaval <45076513+HiteshNaval@users.noreply.github.com>
2024-10-11 21:25:03 +0000

    Update appnavigator..js

--- Froentend/src/navigagtion/appnavigator..js
+++ Froentend/src/navigagtion/appnavigator..js
@@ -1,1 +1,28 @@
+mport React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import TabNavigator from './TabNavigator';
+import LoginScreen from '../screens/LoginScreen';
 
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Main" 
+          component={TabNavigator} 
+          options={{ headerShown: false }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
