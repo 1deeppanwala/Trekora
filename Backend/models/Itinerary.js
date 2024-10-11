e67baebf374de1f126a9f7d507fad26108ec3c6a
Pavanrohith25 <151258697+Pavanrohith25@users.noreply.github.com>
2024-10-11 18:54:31 -0400

    Update Itinerary.js

--- Backend/models/Itinerary.js
+++ Backend/models/Itinerary.js
@@ -1,1 +1,35 @@
+const mongoose = require('mongoose');
 
+const itinerarySchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: true,
+  },
+  destinations: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        required: true,
+      },
+      activities: [
+        {
+          type: String,
+        },
+      ],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create an Itinerary model
+const Itinerary = mongoose.model('Itinerary', itinerarySchema);
+
+module.exports = Itinerary;
