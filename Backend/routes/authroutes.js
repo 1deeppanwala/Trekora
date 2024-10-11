fd4a14c42f8d8cb0ebf4ff1718ed1a7b357021f8
1deeppanwala <145871591+1deeppanwala@users.noreply.github.com>
2024-10-11 18:55:34 -0400

    Update authroutes.js

--- Backend/routes/authroutes.js
+++ Backend/routes/authroutes.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const { login, register } = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware'); // Optional for protected routes
+
+const router = express.Router();
+
+// User Registration Route
+router.post('/register', register);
+
+// User Login Route
+router.post('/login', login);
+
+router.get('/profile', authMiddleware, (req, res) => {
+  res.status(200).json({ message: 'Welcome to your profile!' });
+});
+
+module.exports = router;
 
