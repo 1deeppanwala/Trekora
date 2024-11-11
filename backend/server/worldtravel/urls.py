ff47bfb9fe8ef71f21c631656a0fc834c661f201
PurveshKapadiya <146030138+PurveshKapadiya@users.noreply.github.com>
2024-11-11 17:47:53 -0600

    Update urls.py

--- backend/server/worldtravel/urls.py
+++ backend/server/worldtravel/urls.py
@@ -4,10 +4,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import CountryViewSet, RegionViewSet, VisitedRegionViewSet, regions_by_country, visits_by_country, GeoJSONView
 
+
 router = DefaultRouter()
 router.register(r'countries', CountryViewSet, basename='countries')
 router.register(r'regions', RegionViewSet, basename='regions')
 router.register(r'visitedregion', VisitedRegionViewSet, basename='visitedregion')
+
 
 urlpatterns = [
     path('', include(router.urls)),
