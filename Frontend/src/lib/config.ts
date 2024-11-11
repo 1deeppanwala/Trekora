41515d5bbc23e206d61bc036c446565e97dc0f4c
bnevimal3 <boppudivimal@gmail.com>
2024-11-11 17:48:59 -0500

    Update config.ts

--- Frontend/src/lib/config.ts
+++ Frontend/src/lib/config.ts
@@ -1,4 +1,3 @@
 export let appVersion = 'Web v0.7.0';
-export let versionChangelog = 'https://github.com/seanmorley15/AdventureLog/releases/tag/v0.7.0';
-export let appTitle = 'AdventureLog';
-export let copyrightYear = '2024';
+export let versionChangelog = '';
+export let appTitle = 'Trekora';
