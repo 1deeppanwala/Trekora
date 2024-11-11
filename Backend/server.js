2d42ac3a2b91256415588ddbb0eaddaf233cc936
HiteshNaval <45076513+HiteshNaval@users.noreply.github.com>
2024-11-11 23:44:10 +0000

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,21 +3,33 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
+
+
 const authRoutes = require('./routes/authRoutes');
 const itineraryRoutes = require('./routes/itineraryRoutes');
 
 const app = express();
 
+
+
+
 app.use(cors()); // Enable CORS
 app.use(bodyParser.json()); // Parse JSON request bodies
+
+
 
 const DB_URL = 'mongodb://localhost:27017/trekora'; 
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
 
+
+
 app.use('/api/auth', authRoutes); // Authentication routes
 app.use('/api/itinerary', itineraryRoutes); // Itinerary routes
+
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Trekora API!');
