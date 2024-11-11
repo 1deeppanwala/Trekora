628df5a7a07c141f00897589ba42b0b5ab0ab801
HiteshNaval <45076513+HiteshNaval@users.noreply.github.com>
2024-11-11 23:48:33 +0000

    Update AboutModal.svelte

--- Frontend/src/lib/components/AboutModal.svelte
+++ Frontend/src/lib/components/AboutModal.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 
+
+
         import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
 	import { onMount } from 'svelte';
 	let modal: HTMLDialogElement;
 	import { appVersion, copyrightYear, versionChangelog } from '$lib/config';
+
+
+
 
 	onMount(() => {
 		modal = document.getElementById('my_modal_1') as HTMLDialogElement;
@@ -13,15 +18,22 @@
 		}
 	});
 
+
+
+
 	function close() {
 		dispatch('close');
 	}
+
+
 
 	function handleKeydown(event: KeyboardEvent) {
 		if (event.key === 'Escape') {
 			dispatch('close');
 		}
 	}
+
+
 </script>
 
 <dialog id="my_modal_1" class="modal">
@@ -33,6 +45,9 @@
 				><img src="/favicon.png" alt="Map Logo" class="w-10 -mb-3 ml-2" /></span
 			>
 		</h3>
+
+
+
 		<p class="py-1">
 			Trekora <a
 				target="_blank"
@@ -41,6 +56,9 @@
 				href={versionChangelog}>{appVersion}</a
 			>
 		</p>
+
+
+
 		<p class="py-1">
 			<a
 				href=""
@@ -48,6 +66,8 @@
 				rel="noopener noreferrer"
 				class="text-primary-500 underline"></a
 			>
+
+
 		</p>
 		<p class="py-1"></p>
 		<p class="py-1">
@@ -58,18 +78,27 @@
 				class="text-primary-500 underline"></a
 			>
 		</p>
+
+
 		<p class="py-1"></p>
 		<div class="divider"></div>
 		<h3 class="font-bold text-md"></h3>
 		<p class="py-1 mb-4">
+
+
 			Location Search and Geocoding is provided by <a
 				target="_blank"
 				rel="noopener noreferrer"
+
+
 				class="text-primary-500 underline"
 				href="https://operations.osmfoundation.org/policies/nominatim/">OpenStreepMap</a
 			>. Their data is liscensed under the ODbL license.
 			<br /> Additional attributions can be found in the README file.
 		</p>
+
+
+
 
 		<button class="btn btn-primary" on:click={close}>Close</button>
 	</div>
