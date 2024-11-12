df781c8f06362747727b170e6c7124d7e7407705
HiteshNaval <45076513+HiteshNaval@users.noreply.github.com>
2024-11-12 00:14:22 +0000

    Update UserCard.svelte

--- Frontend/src/lib/components/UserCard.svelte
+++ Frontend/src/lib/components/UserCard.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { continentCodeToString, getFlag } from '$lib';
+	
+
+
+import { continentCodeToString, getFlag } from '$lib';
 	import type { User } from '$lib/types';
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
@@ -10,6 +13,9 @@
 	export let sharing: boolean = false;
 	export let shared_with: string[] | undefined = undefined;
 
+
+
+
 	export let user: User;
 </script>
 
@@ -18,7 +24,10 @@
 >
 	<div class="card-body">
 		<div>
-			{#if user.profile_pic}
+
+
+
+{#if user.profile_pic}
 				<div class="avatar">
 					<div class="w-24 rounded-full">
 						<img src={user.profile_pic} alt={user.username} />
@@ -27,25 +36,41 @@
 			{/if}
 			<h2 class="card-title overflow-ellipsis">{user.first_name} {user.last_name}</h2>
 		</div>
-		<p class="text-sm text-neutral-content">{user.username}</p>
+
+
+
+<p class="text-sm text-neutral-content">{user.username}</p>
 		{#if user.is_staff}
 			<div class="badge badge-primary">Admin</div>
 		{/if}
 		<!-- member since -->
 		<div class="flex items-center space-x-2">
-			<Calendar class="w-4 h-4 mr-1" />
+
+
+
+<Calendar class="w-4 h-4 mr-1" />
 			<p class="text-sm text-neutral-content">
 				{user.date_joined ? 'Joined ' + new Date(user.date_joined).toLocaleDateString() : ''}
 			</p>
 		</div>
 		<div class="card-actions justify-end">
-			{#if !sharing}
+
+
+
+{#if !sharing}
 				<button class="btn btn-primary" on:click={() => goto(`/user/${user.uuid}`)}>View</button>
 			{:else if shared_with && !shared_with.includes(user.uuid)}
 				<button class="btn btn-primary" on:click={() => dispatch('share', user)}>Share</button>
 			{:else}
-				<button class="btn btn-primary" on:click={() => dispatch('unshare', user)}>Unshare</button>
+
+
+
+
+<button class="btn btn-primary" on:click={() => dispatch('unshare', user)}>Unshare</button>
 			{/if}
 		</div>
 	</div>
 </div>
+
+
+
