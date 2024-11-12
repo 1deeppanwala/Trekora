4f5b616b1bf5ce873496d4f5746c9ee861dd9598
HiteshNaval <45076513+HiteshNaval@users.noreply.github.com>
2024-11-12 00:10:38 +0000

    Update NoteCard.svelte

--- Frontend/src/lib/components/NoteCard.svelte
+++ Frontend/src/lib/components/NoteCard.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { addToast } from '$lib/toasts';
+	
+
+
+import { goto } from '$app/navigation';
+
+
+
+import { addToast } from '$lib/toasts';
 	import type { Collection, Note, User } from '$lib/types';
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
 
-	import Launch from '~icons/mdi/launch';
+
+
+
+import Launch from '~icons/mdi/launch';
 	import TrashCan from '~icons/mdi/trash-can';
 	import Calendar from '~icons/mdi/calendar';
 
@@ -14,24 +23,36 @@
 	export let collection: Collection | null = null;
 
 	function editNote() {
-		dispatch('edit', note);
+
+
+
+dispatch('edit', note);
 	}
 
 	async function deleteNote() {
 		const res = await fetch(`/api/notes/${note.id}`, {
 			method: 'DELETE'
-		});
+
+
+
+});
 		if (res.ok) {
 			addToast('success', 'Note deleted successfully');
 			dispatch('delete', note.id);
 		} else {
 			addToast('Failed to delete note', 'error');
-		}
+
+
+
+}
 	}
 </script>
 
 <div
 	class="card w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-md xl:max-w-md overflow-hidden bg-neutral text-neutral-content shadow-xl"
+
+
+
 >
 	<div class="card-body">
 		<div class="flex justify-between">
@@ -40,19 +61,30 @@
 			</h2>
 		</div>
 		<div class="badge badge-primary">Note</div>
-		{#if note.links && note.links.length > 0}
+
+
+
+{#if note.links && note.links.length > 0}
 			<p>{note.links.length} {note.links.length > 1 ? 'Links' : 'Link'}</p>
 		{/if}
 		{#if note.date && note.date !== ''}
 			<div class="inline-flex items-center">
 				<Calendar class="w-5 h-5 mr-1" />
-				<p>{new Date(note.date).toLocaleDateString(undefined, { timeZone: 'UTC' })}</p>
+
+
+
+
+<p>{new Date(note.date).toLocaleDateString(undefined, { timeZone: 'UTC' })}</p>
 			</div>
 		{/if}
 		<div class="card-actions justify-end">
 			<!-- <button class="btn btn-neutral mb-2" on:click={() => goto(`/notes/${note.id}`)}
 				><Launch class="w-6 h-6" />Open Details</button
-			> -->
+
+
+
+
+> -->
 			<button class="btn btn-neutral-200 mb-2" on:click={editNote}>
 				<Launch class="w-6 h-6" />Open
 			</button>
