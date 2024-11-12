9f844501fffedac8ff0d8f72bdaf3e1ca09dffe6
HiteshNaval <45076513+HiteshNaval@users.noreply.github.com>
2024-11-12 00:00:09 +0000

    Update Avatar.svelte

--- Frontend/src/lib/components/Avatar.svelte
+++ Frontend/src/lib/components/Avatar.svelte
@@ -7,10 +7,16 @@
 
 	if (user && !user.first_name && user.username) {
 		letter = user.username[0];
+
+
+
 	}
 </script>
 
 <div class="dropdown dropdown-bottom dropdown-end" tabindex="0" role="button">
+
+
+
 	<div class="avatar placeholder">
 		<div class="bg-neutral rounded-full text-neutral-200 w-10 ml-4">
 			{#if user.profile_pic}
@@ -20,12 +26,19 @@
 			{/if}
 		</div>
 	</div>
+
+
+
 	<!-- svelte-ignore a11y-missing-attribute -->
 	<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 	<ul
 		tabindex="0"
 		class="dropdown-content z-[1] text-neutral-200 menu p-2 shadow bg-neutral mt-2 rounded-box w-52"
 	>
+
+
+
+
 		<!-- svelte-ignore a11y-missing-attribute -->
 		<!-- svelte-ignore a11y-missing-attribute -->
 		<p class="text-lg ml-4 font-bold">Hi, {user.first_name} {user.last_name}</p>
