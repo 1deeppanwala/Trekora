30996310a766cecf29d19041d7d7b3921c188d25
PurveshKapadiya <146030138+PurveshKapadiya@users.noreply.github.com>
2024-11-11 18:01:29 -0600

    Update models.py

--- backend/server/users/models.py
+++ backend/server/users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django_resized import ResizedImageField
 
+
 class CustomUser(AbstractUser):
     profile_pic = ResizedImageField(force_format="WEBP", quality=75, null=True, blank=True, upload_to='profile-pics/')
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
