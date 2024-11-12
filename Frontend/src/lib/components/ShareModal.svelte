01b4371f88b80f4ee0ad14dc00ca26ea215bafb7
HiteshNaval <45076513+HiteshNaval@users.noreply.github.com>
2024-11-12 00:13:05 +0000

    Update ShareModal.svelte

--- Frontend/src/lib/components/ShareModal.svelte
+++ Frontend/src/lib/components/ShareModal.svelte
@@ -3,13 +3,19 @@
 	import { createEventDispatcher } from 'svelte';
 	const dispatch = createEventDispatcher();
 	import { onMount } from 'svelte';
-	import UserCard from './UserCard.svelte';
+	
+
+
+import UserCard from './UserCard.svelte';
 	import { addToast } from '$lib/toasts';
 	let modal: HTMLDialogElement;
 
 	export let collection: Collection;
 
-	let allUsers: User[] = [];
+	
+
+
+let allUsers: User[] = [];
 
 	let sharedWithUsers: User[] = [];
 	let notSharedWithUsers: User[] = [];
@@ -18,12 +24,18 @@
 		let res = await fetch(`/api/collections/${collection.id}/share/${user.uuid}/`, {
 			method: 'POST',
 			headers: {
-				'Content-Type': 'application/json'
+
+
+
+'Content-Type': 'application/json'
 			}
 		});
 		if (res.ok) {
 			sharedWithUsers = sharedWithUsers.concat(user);
-			collection.shared_with.push(user.uuid);
+
+
+
+collection.shared_with.push(user.uuid);
 			notSharedWithUsers = notSharedWithUsers.filter((u) => u.uuid !== user.uuid);
 			addToast('success', `Shared ${collection.name} with ${user.first_name} ${user.last_name}`);
 		}
@@ -31,18 +43,30 @@
 
 	async function unshare(user: User) {
 		let res = await fetch(`/api/collections/${collection.id}/unshare/${user.uuid}/`, {
-			method: 'POST',
+
+
+
+
+method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			}
 		});
 		if (res.ok) {
-			notSharedWithUsers = notSharedWithUsers.concat(user);
+
+
+
+notSharedWithUsers = notSharedWithUsers.concat(user);
 			collection.shared_with = collection.shared_with.filter((u) => u !== user.uuid);
 			sharedWithUsers = sharedWithUsers.filter((u) => u.uuid !== user.uuid);
 			addToast('success', `Unshared ${collection.name} with ${user.first_name} ${user.last_name}`);
 		}
 	}
+
+
+
+
+
 
 	onMount(async () => {
 		modal = document.getElementById('my_modal_1') as HTMLDialogElement;
@@ -50,24 +74,39 @@
 			modal.showModal();
 		}
 		let res = await fetch(`/auth/users/`);
-		if (res.ok) {
+
+
+
+
+if (res.ok) {
 			let data = await res.json();
 			allUsers = data;
-			sharedWithUsers = allUsers.filter((user) => collection.shared_with.includes(user.uuid));
+
+
+
+sharedWithUsers = allUsers.filter((user) => collection.shared_with.includes(user.uuid));
 			notSharedWithUsers = allUsers.filter((user) => !collection.shared_with.includes(user.uuid));
 			console.log(sharedWithUsers);
 			console.log(notSharedWithUsers);
 		}
 	});
 
-	function close() {
+
+
+
+function close() {
 		dispatch('close');
 	}
 
 	function handleKeydown(event: KeyboardEvent) {
 		if (event.key === 'Escape') {
 			dispatch('close');
-		}
+
+
+
+
+
+}
 	}
 </script>
 
